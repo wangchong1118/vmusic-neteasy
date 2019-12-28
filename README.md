546c52e65e5f631e94816fa30b77860f26e9d443
wangchong1118 <wangchong1118@126.com>
2019-12-28 17:25:33 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# vmusic-neteasy
-Vue 项目练习：网易云音乐播放器
+# 简介
+
+> Vue 实战案例演练：Vue 仿制开发网易云音乐
+
+## 测试
+
+``` bash
+# 安装依赖
+npm install
+
+# 本地开发环境：localhost:8080 
+npm strat
+
+# 线上生产环境打包
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      Vue Music 1
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "~common/stylus/variable"
+
+    #app
+      color: $color-theme
+</style>
